0bc5a6bc6f61d42325768201f2945745d7cf9169
Сергей Похабов <itigertechno@gmail.com>
2024-01-08 22:07:42 +0600

    fix bottle

--- src/utils/card-example.ts
+++ src/utils/card-example.ts
@@ -1,10 +1,13 @@
+
+import bottle from '../pictures/bottle.png';
+
 const cardExample = {
     title: "Вино Домен Бастид Жордан Вионье Фют IGP Медитерр...",
     description: "Франция, Cotes du Rhone, белое, сухое, Вионье 100%, 0,75л",
     price: 15000,
     discount: 10000,
     vendorCode: 32130,
-    preview: "https://www.psdmockups.com/wp-content/uploads/2017/01/Wine-Bottle-Tipping-Forward-PSD-Mockup.jpg",
+    preview: bottle,
     stock: false
 }
 
